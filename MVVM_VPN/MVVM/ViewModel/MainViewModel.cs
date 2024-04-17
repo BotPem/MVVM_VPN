984c13940d7f46c44d36e4b7b2ad031b15032707
lalab <lalab@DESKTOP-0900PFO>
2024-04-17 21:50:33 +0300

    Добавьте файлы проекта.

---
+++ MVVM_VPN/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using MVVM_VPN.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MVVM_VPN.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+		//Commands
+		public RelayCommand ShowProtectionView {  get; set; }
+		public RelayCommand ShowSettingsView {  get; set; }
+
+		private object _currentView;
+
+		public object CurrentView
+		{
+			get { return _currentView; }
+			set 
+			{ 
+				_currentView = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public ProtectionViewModel ProtectionVM {  get; set; }
+		public SettingsViewModel SettingsVM { get; set; }
+
+		public MainViewModel()
+		{
+			ProtectionVM = new ProtectionViewModel();
+			SettingsVM = new SettingsViewModel();
+			CurrentView = ProtectionVM;
+
+			ShowProtectionView = new RelayCommand(o => { CurrentView = ProtectionVM; });
+            ShowSettingsView = new RelayCommand(o => { CurrentView = SettingsVM; });
+		}
+	}
+}
